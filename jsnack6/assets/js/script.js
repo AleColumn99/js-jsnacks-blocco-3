0bfea272d4e56b00a3ba89c2686c3047552b19aa
AleColumn99 <alecolumn.99@gmail.com>
2021-04-28 16:46:41 +0200

    esercizio 2

--- jsnack6/assets/js/script.js
+++ jsnack6/assets/js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-  var arr1 = [];
-  var arr2 = [];
+  let arr1 = [];
+  let arr2 = [];
 
   //giusto perché non ho fantasia per farli a mano
-  for (var i=0; i<16; i++) {
+  for (let i=0; i<16; i++) {
     arr1.push(generateRandomNumbers(1,99))
   }
-  for (var i=0; i<11; i++) {
+  for (let i=0; i<11; i++) {
     arr2.push(generateRandomNumbers(1,99))
   }
   console.log(arr1);
   console.log(arr2);
-  var difference = arr1.length - arr2.length;
+  let difference = arr1.length - arr2.length;
   console.log(difference);
-  for (var i=0; i<difference; i++) {
+  for (let i=0; i<difference; i++) {
     arr2.push(generateRandomNumbers(1,99))
   }
   console.log(arr2);
